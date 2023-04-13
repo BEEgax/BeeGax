c9a198319de5c67848072f25a968a8863880a57c
MichaelGloessl04 <gloess190117@sr.htlweiz.at>
2023-04-13 10:34:16 +0200

    structuaral changes

---
+++ BeeGax/src/programs/main/json_handler.cpp
@@ -0,0 +1,36 @@
+#include <ArduinoJson.h>
+#include <TimeLib.h>
+
+#include "include/json_handler.h"
+#include "include/unix.h"
+
+
+StaticJsonDocument<1024> doc;
+JsonArray json_data;
+JsonObject json_entry;
+
+
+void start_time(){
+  setTime(time(NULL));
+}
+
+void init_json(String KEY){
+  doc.clear();
+  doc["key"] = KEY;
+  json_data = doc.createNestedArray("measurements");
+}
+
+String get_json(){
+  String output;
+  serializeJson(doc, output);
+  output = "{\"data\":" + output + "}";
+  return output;
+}
+
+void log_data(int data_type, float content){
+  uint32_t unix = now();
+  json_entry = json_data.createNestedObject();
+  json_entry["value"] = std::to_string(content);
+  json_entry["value_type"] = std::to_string(data_type);
+  json_entry["date"] = get_unix();
+}
