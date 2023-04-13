99b2d80c6eee3b4d48ceae5222be45953555f933
Simon Schafferhofer <schaff190163@sr.htlweiz.at>
2023-04-13 11:31:22 +0200

    rows

--- src/BeescaAPI.js
+++ src/BeescaAPI.js
@@ -1,31 +1,31 @@
 class BeescaAPI {
-    static getHives(){
-        const hives = ["FDFDF", "Volvo", "BMW"];
-        while (hives.length < 8){
-            hives.push("+");
-        };
-        return hives
+  static getHives() {
+    const hives = ["FDFDF", "Volvo", "BMW"];
+    while (hives.length < 8) {
+      hives.push("+");
     }
+    return hives;
+  }
 
-    static getWeight(){
-        const weight = 0.0
-        return weight
-    }
+  static getWeight() {
+    const weight = 0.0;
+    return weight;
+  }
 
-    static getTemperature(){
-        const temperature = 0.0
-        return temperature
-    }
+  static getTemperature() {
+    const temperature = 0.0;
+    return temperature;
+  }
 
-    static getHumidity(){
-        const humidity = 0.0
-        return humidity
-    }
+  static getHumidity() {
+    const humidity = 0.0;
+    return humidity;
+  }
 
-    static getLocation(){
-        const location = ""
-        return location
-    }
+  static getLocation() {
+    const location = "";
+    return location;
+  }
 }
 
 export default BeescaAPI;
--- src/HivesPadge.js
+++ src/HivesPadge.js
@@ -1,87 +1,67 @@
 import React, { useState } from "react";
-import {View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button } from "react-native";
 import BeescaAPI from "./BeescaAPI";
-
 
 const HivesPadge = ({ navigation }) => {
   const POPUPconf = (buttonText) => {
-    navigation.navigate('POPUP', {buttonID: buttonText});
+    navigation.navigate("POPUP", { buttonID: buttonText });
     const newList = [];
     buttonList.map((i) => {
-        newList.push(i);
+      newList.push(i);
     });
-    newList.pop()
-    newList.push("gaxe")
-    setButtonList(BeescaAPI.getHives())
+    newList.pop();
+    newList.push("gaxe");
+    setButtonList(BeescaAPI.getHives());
   };
   const [buttonList, setButtonList] = useState(BeescaAPI.getHives());
 
   const renderButton = (buttonText) => {
-    return <Button title={buttonText} onPress={() => POPUPconf(buttonText)} />;
-  };
     return (
+      <View style={styles.buttonContainer}>
+        <Button title={buttonText} onPress={() => POPUPconf(buttonText)} />
+      </View>
+    );
+  };
+
+  return (
     <View style={styles.container}>
-      <Text style={styles.text}>Welcome to the settings page!</Text>
-      {buttonList.map((buttonText) => (
-      renderButton(buttonText)
-    ))}
+      <Text style={styles.text}>Your Beehives:</Text>
+      <View style={styles.row}>
+        <View style={styles.column}>
+          {buttonList.slice(0, 4).map((buttonText) => renderButton(buttonText))}
+        </View>
+        <View style={styles.column}>
+          {buttonList.slice(4, 8).map((buttonText) => renderButton(buttonText))}
+        </View>
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   text: {
     fontSize: 24,
-    marginBottom: 10,
+    marginBottom: 100,
   },
-  container: {
-    flex: 1,
+  row: {
+    flexDirection: "row",
     justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF",
   },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-    marginBottom: 20,
+  column: {
+    flexDirection: "column",
+    alignItems: "center",
   },
   buttonContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    position: "absolute",
-    bottom: 0,
-    left: 0,
-    right: 0,
-    height: 60,
-    backgroundColor: "#FFFFFF",
-    shadowOffset: {
-      width: 0,
-      height: -3,
-    },
-  },
-  button: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#BBBBBB",
-    height: "100%",
-  },
-  activeButton: {
-    backgroundColor: "#FFDB58",
-  },
-  buttonText: {
-    fontSize: 16,
-    color: "#FFFFFF",
-    marginLeft: 5,
-  },
-  activeButtonText: {
-    color: "#FFFFFF",
+    width: 100, // set a fixed width
+    height: 100, // set a fixed height
+    margin: 10,
+    borderRadius: 10,
+    overflow: "hidden",
   },
 });
 
