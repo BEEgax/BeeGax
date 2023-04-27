176412172d304dec5a50a10899be6c587b72d9c8
Simon Schafferhofer <schaff190163@sr.htlweiz.at>
2023-04-27 10:44:40 +0200

    updated

--- src/BeescaAPI.js
+++ src/BeescaAPI.js
@@ -1,31 +1,32 @@
 class BeescaAPI {
-  static getHives() {
-    const hives = ["FDFDF", "Volvo", "BMW"];
-    while (hives.length < 8) {
-      hives.push("+");
+    static hives = ["Hive1", "Hive2", "Hive3", "Hive5"];
+    
+    static getHives(){
+        while (BeescaAPI.hives.length < 8){
+            BeescaAPI.hives.push("+");
+        };
+        return BeescaAPI.hives
     }
-    return hives;
-  }
 
-  static getWeight() {
-    const weight = 0.0;
-    return weight;
-  }
+    static getWeight(){
+        const weight = 0.0
+        return weight
+    }
 
-  static getTemperature() {
-    const temperature = 0.0;
-    return temperature;
-  }
+    static getTemperature(){
+        const temperature = 0.0
+        return temperature
+    }
 
-  static getHumidity() {
-    const humidity = 0.0;
-    return humidity;
-  }
+    static getHumidity(){
+        const humidity = 0.0
+        return humidity
+    }
 
-  static getLocation() {
-    const location = "";
-    return location;
-  }
+    static getLocation(){
+        const location = ""
+        return location
+    }
 }
 
 export default BeescaAPI;
